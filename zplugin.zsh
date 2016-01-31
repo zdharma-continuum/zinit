9c48b2670ecae32449dcecfe313f26aacd80faa8
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-01-31 20:53:18 +0100

    Support deleting range bindkeys, with or without -M

--- zplugin.zsh
+++ zplugin.zsh
@@ -233,15 +233,17 @@ ZPLG_COL=(
     # with no options or with -s, plus possible -M
     # option
     local -A optsA
-    zparseopts -A optsA -D ${(s::):-lLdDANmrseva} "M:"
+    zparseopts -A optsA -D ${(s::):-lLdDANmrsevaR} "M:"
 
     local -a opts
     opts=( "${(k)optsA[@]}" )
 
     if [[ "${#opts[@]}" -eq "0" ||
         ( "${#opts[@]}" -eq "1" && "${opts[(r)-M]}" = "-M" ) ||
+        ( "${#opts[@]}" -eq "1" && "${opts[(r)-R]}" = "-R" ) ||
         ( "${#opts[@]}" -eq "1" && "${opts[(r)-s]}" = "-s" ) ||
-        ( "${#opts[@]}" -le "2" && "${opts[(r)-M]}" = "-M" && "${opts[(r)-s]}" = "-s" )
+        ( "${#opts[@]}" -le "2" && "${opts[(r)-M]}" = "-M" && "${opts[(r)-s]}" = "-s" ) ||
+        ( "${#opts[@]}" -le "2" && "${opts[(r)-M]}" = "-M" && "${opts[(r)-R]}" = "-R" )
     ]]; then
         local string="${(q)1}" widget="${(q)2}"
         local quoted
@@ -257,6 +259,21 @@ ZPLG_COL=(
             quoted="$string $widget $Mopt $Marg"
         else
             quoted="$string $widget"
+        fi
+
+        # -R given?
+        if [ "${opts[(r)-R]}" = "-R" ]; then
+            local Ropt="-R"
+            Ropt="${(q)Ropt}"
+
+            if [ "${opts[(r)-M]}" = "-M" ]; then
+                quoted="$quoted $Ropt"
+            else
+                # Two empty fields for non-existent -M arg
+                local space="_"
+                space="${(q)space}"
+                quoted="$quoted $space $space $Ropt"
+            fi
         fi
 
         quoted="${(q)quoted}"
@@ -1589,14 +1606,21 @@ ZPLG_COL=(
         sw_arr=( "${(z)sw}" )
 
         # Remove one level of quoting to pass to bindkey
-        sw_arr[1]="${(Q)sw_arr[1]}"
-        sw_arr[2]="${(Q)sw_arr[2]}"
-        sw_arr[3]="${(Q)sw_arr[3]}"
-        sw_arr[4]="${(Q)sw_arr[4]}"
+        sw_arr[1]="${(Q)sw_arr[1]}" # Keys
+        sw_arr[2]="${(Q)sw_arr[2]}" # Widget
+        sw_arr[3]="${(Q)sw_arr[3]}" # Optional -M
+        sw_arr[4]="${(Q)sw_arr[4]}" # Optional map name
+        sw_arr[5]="${(Q)sw_arr[5]}" # Optional -R
 
-        if [ "${sw_arr[3]}" = "-M" ]; then
+        if [[ "${sw_arr[3]}" = "-M" && "${sw_arr[5]}" != "-R" ]]; then
             print "Deleting bindkey ${sw_arr[1]} ${sw_arr[2]} ${ZPLG_COL[info]}mapped to ${sw_arr[4]}$reset_color"
             bindkey -M "${sw_arr[4]}" -r "${sw_arr[1]}"
+        elif [[ "${sw_arr[3]}" = "-M" && "${sw_arr[5]}" = "-R" ]]; then
+            print "Deleting ${ZPLG_COL[info]}range$reset_color bindkey ${sw_arr[1]} ${sw_arr[2]} ${ZPLG_COL[info]}mapped to ${sw_arr[4]}$reset_color"
+            bindkey -M "${sw_arr[4]}" -Rr "${sw_arr[1]}"
+        elif [[ "${sw_arr[3]}" != "-M" && "${sw_arr[5]}" = "-R" ]]; then
+            print "Deleting ${ZPLG_COL[info]}range$reset_color bindkey ${sw_arr[1]} ${sw_arr[2]}" 
+            bindkey -Rr "${sw_arr[1]}"
         else
             print "Deleting bindkey ${sw_arr[1]} ${sw_arr[2]}"
             bindkey -r "${sw_arr[1]}"
