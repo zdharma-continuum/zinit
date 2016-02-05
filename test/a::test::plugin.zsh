be5be75c536f3c8b4eee363eb52751927680cc01
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-02-05 13:29:50 +0100

    test1 shows initial option values, for easy unload issues recognition

--- test/a::test::plugin.zsh
+++ test/a::test::plugin.zsh
@@ -26,9 +26,14 @@ alias aaa2=bbb
 alias -s gif=iv
 alias -g LLL="|less"
 
+[[ -n ${-[(r)9]} ]] && echo "Initially it's -9 (auto_list) set" || echo "Initially it's -9 (auto_list) unset"
 setopt +9
+[[ -n ${-[(r)w]} ]] && echo "Initially it's -w (chase_links) set" || echo "Initially it's -w (chase_links) unset"
 setopt -w
+[[ -o "menucomplete" ]] && echo "Initially it's menucomplete set" || echo "Initially it's menucomplete unset"
 setopt menucomplete
+[[ -o "listtypes" ]] && echo "Initially it's listtypes set" || echo "Initially it's listtypes unset"
+[[ -o "listpacked" ]] && echo "Initially it's listpacked set" || echo "Initially it's listpacked unset"
 setopt -o nolist_types -o LIST_PACKED
 #setopt -o automenu posix_cd -oautocd
 #setopt \'pushd_minus
