c30581e604e42edd589771e5117c6489f5023230
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-02-07 19:26:49 +0100

    Tests: second working emulate sh test

---
+++ test/sh_test2.zsh
@@ -0,0 +1,38 @@
+#!/bin/zsh
+
+#
+# Do some setup
+# Script can be run from arbitrary directory under zplugin/ tree
+#
+
+emulate sh
+source "`pwd`/${0:h}/tinclude.zsh" "$0" || exit 1
+
+#
+# Start Dtrace
+#
+
+---start
+
+###
+### Test body
+###
+
+local PLUGIN_NAME="_local/safe-paste"
+zplugin load "$PLUGIN_NAME"
+
+#
+# Stop Dtrace
+#
+
+---stop
+
+###
+### Gather and compare results
+###
+
+zplugin report "$PLUGIN_NAME" > "$___TEST_REPORT_FILE" 2>&1
+zplugin unload "$PLUGIN_NAME" > "$___TEST_UNLOAD_FILE" 2>&1
+
+---compare
+---end
