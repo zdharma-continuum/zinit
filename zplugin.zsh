f0c7d9acbfb63c78b1e50695b96207b913f15d62
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-06-06 00:57:39 +0200

    More logical mock-plugin creation

--- zplugin.zsh
+++ zplugin.zsh
@@ -1647,6 +1647,10 @@ builtin setopt noaliases
         command mkdir "$ZPLG_PLUGINS_DIR"
         # For compaudit
         command chmod go-w "$ZPLG_PLUGINS_DIR"
+
+        # Prepare mock-like plugin for Zplugin itself
+        command mkdir "$ZPLG_PLUGINS_DIR/_local---zplugin"
+        command ln -s "$ZPLG_DIR/_zplugin" "$ZPLG_PLUGINS_DIR/_local---zplugin"
     }
     [ ! -d "$ZPLG_COMPLETIONS_DIR" ] && {
         command mkdir "$ZPLG_COMPLETIONS_DIR"
@@ -1654,8 +1658,6 @@ builtin setopt noaliases
         command chmod go-w "$ZPLG_COMPLETIONS_DIR"
 
         # Symlink _zplugin completion into _local---zplugin directory
-        command mkdir "$ZPLG_PLUGINS_DIR/_local---zplugin"
-        command ln -s "$ZPLG_DIR/_zplugin" "$ZPLG_PLUGINS_DIR/_local---zplugin"
         command ln -s "$ZPLG_PLUGINS_DIR/_local---zplugin/_zplugin" "$ZPLG_COMPLETIONS_DIR"
     }
     [ ! -d "$ZPLG_SNIPPETS_DIR" ] && {
