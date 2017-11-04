9a349f2cae03e28347053c1b0649077b609fb752
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2017-11-04 11:26:09 +0100

    *autoload: New *-cd(), understands SVN and non-SVN snippets

--- zplugin-autoload.zsh
+++ zplugin-autoload.zsh
@@ -1794,44 +1794,43 @@ ZPLGM[EXTENDED_GLOB]=""
 # $1 - plugin spec (4 formats: user---plugin, user/plugin, user, plugin)
 # $2 - plugin (only when $1 - i.e. user - given)
 -zplg-cd() {
+    setopt localoptions extendedglob
 
     if [[ "$1" = (http|https|ftp|ftps|scp)://* || "$1" = OMZ::* || "$1" = PZT::* ]]; then
         integer MBEGIN MEND
-        local url1="$1" url2 filename local_dir MATCH
-        filename="${${url1:t}%%\?*}"
+        local url="$1" url1 url2 filenameA filenameB filename0A filename0B local_dirA local_dirB MATCH
 
-        url1="${url1%/}" url2="$url1"
+        # Remove leading whitespace and trailing /
+        url="${${url#"${url%%[! $'\t']*}"}%/}"
+        url1="$url" url2="$url"
 
-        # Oh-My-Zsh shorthand
         local -A sice
         local -a tmp
-        tmp=( "${(z@)ZPLG_SICE[$url1/]}" )
+        tmp=( "${(z@)ZPLG_SICE[$url/]}" )
         (( ${#tmp} > 1 && ${#tmp} % 2 == 0 )) && sice=( "${tmp[@]}" )
 
-        (( ${+sice[svn]} )) && {
-            # SVN mirroring
-            url1="${url1/OMZ::/https://github.com/robbyrussell/oh-my-zsh/trunk/}"
-            url1="${url1/PZT::/https://github.com/sorin-ionescu/prezto/trunk/}"
+        url1[1,5]="${ZPLG_1MAP[${url[1,5]}]:-$url[1,5]}" # svn
+        url2[1,5]="${ZPLG_2MAP[${url[1,5]}]:-$url[1,5]}" # normal
 
-            # Construct a local directory name from what's in url
-            local_dir="${${url1%/*}//(#m)(http|https|ftp|ftps|scp):\/\//${MATCH%???}--}"
-            local_dir="${${local_dir//\//--S--}//=/--EQ--}"
-            local_dir="${${local_dir//\?/--QM--}//\&/--AMP--}"
-
-            [[ -e "${ZPLGM[SNIPPETS_DIR]}/$local_dir" ]] && builtin cd "${ZPLGM[SNIPPETS_DIR]}/$local_dir"
-            return
-        }
-
-        # Normal download via wget, etc.
-        url2="${url2/OMZ::/https://github.com/robbyrussell/oh-my-zsh/raw/master/}"
-        url2="${url2/PZT::/https://github.com/sorin-ionescu/prezto/raw/master/}"
+        filenameA="${${url1%%\?*}:t}"
+        filename0A="${${${url1%%\?*}:h}:t}"
+        filenameB="${${url2%%\?*}:t}"
+        filename0B="${${${url2%%\?*}:h}:t}"
 
         # Construct a local directory name from what's in url
-        local_dir="${${url2%/*}//(#m)(http|https|ftp|ftps|scp):\/\//${MATCH%???}--}"
-        local_dir="${${local_dir//\//--S--}//=/--EQ--}"
-        local_dir="${${local_dir//\?/--QM--}//\&/--AMP--}"
+        local_dirA="${${url1%/*}//(#m)(http|https|ftp|ftps|scp):\/\//${MATCH%???}--}"
+        local_dirA="${${${${local_dirA//\//--}//=/--EQ--}//\?/--QM--}//\&/--AMP--}"
+        local_dirA="${ZPLGM[SNIPPETS_DIR]}/$local_dirA"
 
-        builtin cd "${ZPLGM[SNIPPETS_DIR]}/$local_dir"
+        local_dirB="${${url2%/*}//(#m)(http|https|ftp|ftps|scp):\/\//${MATCH%???}--}"
+        local_dirB="${${${${local_dirB//\//--}//=/--EQ--}//\?/--QM--}//\&/--AMP--}"
+        local_dirB="${ZPLGM[SNIPPETS_DIR]}/${local_dirB%--$filename0B}/$filename0B"
+
+        [[ "${+sice[svn]}" = "1" || -e "$local_dirA/$filenameA" ]] && {
+            [[ -e "$local_dirA/$filenameA" ]] && builtin cd "$local_dirA/$filenameA" || echo "No such snippet"
+        } || {
+            [[ -e "$local_dirB" ]] && builtin cd "$local_dirB" || echo "No such snippet"
+        }
     else
         -zplg-any-to-user-plugin "$1" "$2"
         local user="${reply[-2]}" plugin="${reply[-1]}"
