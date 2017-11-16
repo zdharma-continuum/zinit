71407901cfb2486035cb7a2350b61ec9a7ac006d
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2017-11-16 11:56:52 +0100

    test: Remove old testing framework

--- test/tzplugin/plugins/_local---zsh-colors/colors.plugin.zsh
+++
@@ -1,8 +0,0 @@
-# Content based a bit on this nice documentation:
-#
-# https://wiki.archlinux.org/index.php/Zsh
-#
-# You can also debug this library with `whence -f red`
-colors=( black red green yellow blue magenta cyan white )
-autoload -Uz "${colors[@]}"
-autoload -Uz colors && colors
