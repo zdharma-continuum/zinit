1a3d3ba82fd8728dfe221a869e1163d05cb84b4c
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2017-11-18 08:02:39 +0100

    test: Save Zplugin state after test has been run for comparison. Uplifts

--- test/bin/runtest.zsh
+++ test/bin/runtest.zsh
@@ -9,6 +9,12 @@ emulate -LR "$emul" -o warncreateglobal -o typesetsilent -o extendedglob
 [[ -d $PWD/$1/answer ]] && rm -rf $PWD/$1/answer
 [[ -f $PWD/$1/state ]] && rm -f $PWD/$1/state
 
+# Discard per-setup (e.g. per-version) PATH and FPATH entries
+builtin autoload +X is-at-least
+builtin autoload +X allopt
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin
+FPATH=/usr/share/zsh/site-functions:/usr/local/share/zsh/functions:/usr/local/share/zsh/site-functions
+
 # Setup paths and load Zplugin
 local REPLY p
 local -a reply
@@ -16,7 +22,7 @@ local -A ZPLGM
 ZPLGM[BIN_DIR]=$PWD
 command mkdir -p $PWD/$1/answer
 ZPLGM[HOME_DIR]=$PWD/$1/answer
-source ${ZPLGM[BIN_DIR]}/zplugin.zsh
+builtin source "${ZPLGM[BIN_DIR]}/zplugin.zsh"
 
 # FUNCTION: internet_mock_git {{{
 internet_mock_git() {
@@ -61,6 +67,8 @@ tst_verbosity() {
 # }}}
 # FUNCTION: store_state {{{
 store_state() {
+    (( ${+functions[-zplg-diff-env-compute]} )) || builtin source ${ZPLGM[BIN_DIR]}"/zplugin-autoload.zsh"
+
     local -A mymap
     mymap=( "${(kv@)ZPLGM}" )
     local -a keys
@@ -79,14 +87,49 @@ store_state() {
     ZPLGM=( "${(kv)ZPLGM[@]/${PWD:h}\//}" )
     ZPLGM=( "${(kv)ZPLGM[@]/${${PWD:h}:h}\//}" )
 
-    print -rl -- "${(kv@)ZPLGM}" >! answer/state
+    print -rl -- "${(kv@)ZPLGM}" "---" >! answer/state
+
+    print -r -- "ERRORS" >! answer/errors
+
+    # Normalize PATH and FPATH
+    path=( "${path/${PWD:h}\//}" )
+    path=( "${path/${${PWD:h}:h}\//}" )
+    fpath=( "${fpath/${PWD:h}\//}" )
+    fpath=( "${fpath/${${PWD:h}:h}\//}" )
+    ZPLG_PATH_BEFORE=( "${(kv@)ZPLG_PATH_BEFORE/${PWD:h}\//}" )
+    ZPLG_PATH_BEFORE=( "${(kv@)ZPLG_PATH_BEFORE/${${PWD:h}:h}\//}" )
+    ZPLG_PATH_AFTER=( "${(kv@)ZPLG_PATH_AFTER/${PWD:h}\//}" )
+    ZPLG_PATH_AFTER=( "${(kv@)ZPLG_PATH_AFTER/${${PWD:h}:h}\//}" )
+    ZPLG_FPATH_BEFORE=( "${(kv@)ZPLG_FPATH_BEFORE/${PWD:h}\//}" )
+    ZPLG_FPATH_BEFORE=( "${(kv@)ZPLG_FPATH_BEFORE/${${PWD:h}:h}\//}" )
+    ZPLG_FPATH_AFTER=( "${(kv@)ZPLG_FPATH_AFTER/${PWD:h}\//}" )
+    ZPLG_FPATH_AFTER=( "${(kv@)ZPLG_FPATH_AFTER/${${PWD:h}:h}\//}" )
+
+    for k in "${ZPLG_REGISTERED_PLUGINS[@]}"; do
+        -zplg-diff-functions-compute "$k" || print -r -- "Failed: -zplg-diff-functions-compute for $k" >>! answer/errors
+        -zplg-diff-options-compute "$k"   || print -r -- "Failed: -zplg-diff-options-compute for $k" >>! answer/errors
+        -zplg-diff-env-compute "$k"       || print -r -- "Failed: -zplg-diff-env-compute $k" >>! answer/errors
+        -zplg-diff-parameter-compute "$k" || print -r -- "Failed: -zplg-diff-parameter-compute $k" >>! answer/errors
+    done
+
+    keys=( ZPLG_REGISTERED_PLUGINS ZPLG_REGISTERED_STATES ZPLG_SNIPPETS
+           ZPLG_SICE ZPLG_FUNCTIONS ZPLG_OPTIONS ZPLG_PATH ZPLG_FPATH
+           ZPLG_PARAMETERS_PRE ZPLG_PARAMETERS_POST ZPLG_ZSTYLES ZPLG_BINDKEYS
+           ZPLG_ALIASES ZPLG_WIDGETS_SAVED ZPLG_WIDGETS_DELETE ZPLG_COMPDEF_REPLAY
+           ZPLG_CUR_PLUGIN
+    )
+    for k in "${keys[@]}"; do
+        print -r -- "$k: ${(qq@)${(Pkv@)k}}" >>! answer/state
+    done
+
+    print -rl -- "---" "Parameter module: ${+modules[zsh/parameter]}" >>! answer/state
 }
 # }}}
 
 builtin cd "$1"
-[[ -n "$2" ]] && typeset -g VERBOSE=1
-[[ -n "$3" ]] && typeset -g DBG=1                          # $(DEBUG)
-[[ -n "$4" ]] && { autoload allopt; allopt > allopt.txt; } # $(OPTDUMP)
+[[ -n "$2" ]] && typeset -g VERBOSE=1     # $(VERBOSE)
+[[ -n "$3" ]] && typeset -g DBG=1         # $(DEBUG)
+[[ -n "$4" ]] && { allopt > allopt.txt; } # $(OPTDUMP)
 
 local -a plugins
 [[ -f "plugins" ]] && plugins=( "${(@f)"$(<./plugins)"}" )
