59c426f71bd2185630270d35498cbf82405a21e7
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-06-22 19:09:54 +0200

    zplugin: Remove debug messages from -zplg-deploy-message

--- zplugin.zsh
+++ zplugin.zsh
@@ -1432,7 +1432,6 @@ builtin setopt noaliases
             local alltext text IFS=$'\n' nl=$'\n'
             repeat 25; do read -u"$1" text; alltext+="${text:+$text$nl}"; done
             [[ -n "$alltext" ]] && zle -M "$alltext"
-            print -rl "Yes outputted the text: $alltext" >> /tmp/reply
         }
         zle -F "$1"; exec {1}<&-
         return 0
@@ -1442,7 +1441,6 @@ builtin setopt noaliases
     # it, otherwise substitute 0
     exec {THEFD} < <(LANG=C sleep $(( 0.01 + ${${${(M)1#@sleep:}:+${1#@sleep:}}:-0} )); print -r -- ${1:#(@msg|@sleep:*)} "${@[2,-1]}")
     zle -F "$THEFD" -zplg-deploy-message
-    print -rl "Yes setupt the text: $alltext" >> /tmp/reply
 }
 # }}}
 
