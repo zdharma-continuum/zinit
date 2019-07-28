ab0bd48431bc159abbccddcf7fed782547a802a0
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-07-28 17:48:52 +0200

    deploy: Send the confirmation email also to @zdharma.org

--- share/deploy_doc.sh
+++ share/deploy_doc.sh
@@ -81,4 +81,4 @@ git push "$SSH_REPO" "$TARGET_BRANCH"
 
 rm -f ../share/deploy_key
 
-mail -s "The Zplugin deploy done on $( date '+%m/%d/%Y' )" sgniazdowski@gmail.com <<< 'The deploy has been done'
+mail -c psprint@zdharma.org -s "The Zplugin deploy done on $( date '+%m/%d/%Y' )" sgniazdowski@gmail.com <<< 'The deploy has been done'
