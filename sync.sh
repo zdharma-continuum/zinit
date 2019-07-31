a0fad061e62c7f99464ef4313dffa8fbca3a81ee
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-07-31 15:24:57 +0200

    sync.sh: first pull in the zplugin_readme repository

--- sync.sh
+++ sync.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env zsh
 
-cp -vf ~/github/zplugin_readme/docs/*.md docs
+autoload colors
+colors
+
+git -C ~/github/zplugin_readme checkout gh-pages || {
+    print $fg_bold[green]Couldn\'t checkout, trying stash first...$reset_color
+    sleep 1
+    git -C ~/github/zplugin_readme stash
+    git -C ~/github/zplugin_readme checkout gh-pages || exit 1
+}
+git -C ~/github/zplugin_readme llf
+git -C ~/github/zplugin_readme rebase || exit 2
+
+print $fg_bold[green]Copying files...$reset_color
+sleep 1
+cp -f ~/github/zplugin_readme/docs/*.md docs
 git add -A docs
 cp -vf ~/github/zplugin_readme/mkdocs.yml .
 git add mkdocs.yml
+print $fg_bold[green]Commiting:$reset_color
+sleep 1
 git commit ${${(M)1:#(a|am|amend)}:+--amend} && \
     git push -f origin documentation && \
         make gh-pages
