4c379c7c036ac7093641d1aad27f8c588a864e02
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-07-31 02:50:41 +0200

    sync.sh

---
+++ sync.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+cp -vf ~/github/zplugin_readme/docs/*.md docs
+git add -A docs
+cp -vf ~/github/zplugin_readme/mkdocs.yml .
+git add mkdocs.yml
+git commit ${${(M)1:#(a|am|amend)}:+--amend} && \
+    git push origin documentation && \
+        make gh-pages
