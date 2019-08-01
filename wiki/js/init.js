d412bbfc97f81c5bcf140022b449653868e47842
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-08-01 20:31:12 +0200

    Site build [08/01/2019 20:31:12]

--- wiki/js/init.js
+++ wiki/js/init.js
@@ -1,5 +1,7 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  document.querySelectorAll('code').forEach((block) => {
-    hljs.highlightBlock(block);
-  });
-});
+function highlightCode() {
+  var pres = document.querySelectorAll("pre>code");
+  for (var i = 0; i < pres.length; i++) {
+    hljs.highlightBlock(pres[i]);
+  }
+}
+highlightCode();
