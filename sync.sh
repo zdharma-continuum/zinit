f8a296ad20bfff144f9d96f249faefebf2f6297c
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-08-15 04:27:21 +0200

    sync.sh: Copy the material theme

--- sync.sh
+++ sync.sh
@@ -6,13 +6,13 @@ colors
 git -C ~/github/zplugin_readme checkout gh-pages || {
     print $fg_bold[green]Couldn\'t checkout, trying stash first...$reset_color
     sleep 1
-    git -C ~/github/zplugin_readme stash
+    git -C ~/github/zplugin_readme stash save "$(LANG=C date)"
     git -C ~/github/zplugin_readme checkout gh-pages || exit 1
 }
 git -C ~/github/zplugin_readme llf
 git -C ~/github/zplugin_readme rebase || {
     print $fg_bold[green]Couldn\'t rebase, trying stash first...$reset_color
-    git -C ~/github/zplugin_readme stash || exit 2
+    git -C ~/github/zplugin_readme stash save "$(LANG=C date)" || exit 2
     git -C ~/github/zplugin_readme rebase || exit 3
 }
 
@@ -29,6 +29,8 @@ cp -fv ~/github/zplugin_readme/docs/js/* docs/js
 git add -A docs/js
 cp -fv ~/github/zplugin_readme/docs/img/* docs/img
 git add -A docs/img
+cp -vR ~/github/zplugin_readme/docs/theme/ docs/theme
+git add -A docs/theme
 print $fg_bold[green]Commiting:$reset_color
 sleep 1
 { git commit ${${(M)1:#(a|am|amend)}:+--amend} || git status } && \
