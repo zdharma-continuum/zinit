049a8a21ef527c71120af4c43dd775ce09f3e386
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-08-30 15:55:14 +0200

    *autoload: *create – use the $0-handling from the recent plugin standard

--- zplugin-autoload.zsh
+++ zplugin-autoload.zsh
@@ -2608,6 +2608,7 @@ ZPLGM[EXTENDED_GLOB]=""
 # http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
 
 0="\${\${ZERO:-\${0:#\$ZSH_ARGZERO}}:-\${(%):-%N}}"
+0="\${\${(M)0:#/*}:-\$PWD/\$0}"
 
 # Then \${0:h} to get plugin's directory
 EOF
