639e176a4fb3f043991b1151641681b8694ce868
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-09-10 06:39:31 +0200

    install.sh: New variable ZPLG_BIN_DIR_NAME

--- doc/install.sh
+++ doc/install.sh
@@ -8,9 +8,18 @@ if [ -z "$ZPLG_HOME" ]; then
     ZPLG_HOME="${ZDOTDIR:-$HOME}/.zplugin"
 fi
 
+if [ -z "$ZPLG_BIN_DIR_NAME" ]; then
+    ZPLG_BIN_DIR_NAME="bin"
+fi
+
 if ! test -d "$ZPLG_HOME"; then
     mkdir "$ZPLG_HOME"
     chmod g-rwX "$ZPLG_HOME"
+fi
+
+if ! command -v git >/dev/null 2>&1; then
+    echo "[1;31m❯❯❯[0m Something went wrong: no [1;32mgit[0m available, cannot proceed"
+    exit 1
 fi
 
 # Get the download-progress bar tool
@@ -27,20 +36,20 @@ elif command -v wget >/dev/null 2>&1; then
 fi
 
 echo
-echo "[1;34m❯❯❯[0m Installing zplugin at [1;35m$ZPLG_HOME/bin[0m"
-if test -d "$ZPLG_HOME/bin/.git"; then
-    cd "$ZPLG_HOME/bin"
+echo "[1;34m❯❯❯[0m Installing zplugin at [1;35m$ZPLG_HOME/$ZPLG_BIN_DIR_NAME[0m"
+if test -d "$ZPLG_HOME/$ZPLG_BIN_DIR_NAME/.git"; then
+    cd "$ZPLG_HOME/$ZPLG_BIN_DIR_NAME"
     git pull origin master
 else
     cd "$ZPLG_HOME"
-    { git clone --progress https://github.com/zdharma/zplugin.git bin \
+    { git clone --progress https://github.com/zdharma/zplugin.git "$ZPLG_BIN_DIR_NAME" \
         2>&1 | { /tmp/zplugin/git-process-output.zsh || cat; } } 2>/dev/null
-    if [ -d bin ]; then
+    if [ -d "$ZPLG_BIN_DIR_NAME" ]; then
         echo
-        echo "[1;34m❯❯❯[0m Zplugin succesfully installed at [1;32m$ZPLG_HOME/bin[0m"
+        echo "[1;34m❯❯❯[0m Zplugin succesfully installed at [1;32m$ZPLG_HOME/$ZPLG_BIN_DIR_NAME[0m"
     else
         echo
-        echo "[1;31m❯❯❯[0m Something went wrong, couldn't install Zplugin at [1;33m$ZPLG_HOME/bin[0m"
+        echo "[1;31m❯❯❯[0m Something went wrong, couldn't install Zplugin at [1;33m$ZPLG_HOME/$ZPLG_BIN_DIR_NAME[0m"
     fi
 fi
 
@@ -57,7 +66,7 @@ echo "[34m❯❯❯[0m Updating $THE_ZDOTDIR/.zshrc (3 lines of code, at the b
 cat <<-EOF >> "$THE_ZDOTDIR/.zshrc"
 
 ### Added by Zplugin's installer
-source '$ZPLG_HOME/bin/zplugin.zsh'
+source '$ZPLG_HOME/$ZPLG_BIN_DIR_NAME/zplugin.zsh'
 autoload -Uz _zplugin
 (( \${+_comps} )) && _comps[zplugin]=_zplugin
 ### End of Zplugin installer's chunk
