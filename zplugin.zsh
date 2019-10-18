7693af08620abeffad46cc831c40e749b5fa499c
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-10-18 17:55:45 +0200

    zplugin: Remove stray whitespace

--- zplugin.zsh
+++ zplugin.zsh
@@ -1699,7 +1699,7 @@ aliases${~exts})(*) ]] && ZPLG_ICES[${match[1]}]+="${ZPLG_ICES[${match[1]}]:+;}$
     for __task in "${ZPLG_RUN[@]}"; do
         -zplg-run-task 1 "${(@z)__task}" && ZPLG_TASKS+=( "$__task" )
         [[ $(( ++__idx, __count += ${${REPLY:+1}:-0} )) -gt 0 && "$1" != "burst" ]] && \
-            { 
+            {
                 ANFD="13371337" # for older Zsh + noclobber option
                 exec {ANFD}< <(LANG=C command sleep 0.002; builtin print run;)
                 command true # workaround a Zsh bug, see: http://www.zsh.org/mla/workers/2018/msg00966.html
