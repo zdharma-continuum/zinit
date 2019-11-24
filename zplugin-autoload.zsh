4e0e48736fa427cf84dc9c8bea822b8dfae96b56
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-11-24 06:52:01 +0100

    *autoload: `create' adds unquoted, not quoted 0=… snippet

--- zplugin-autoload.zsh
+++ zplugin-autoload.zsh
@@ -2538,8 +2538,8 @@ ZPLGM[EXTENDED_GLOB]=""
 # According to the Zsh Plugin Standard:
 # http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
 
-0="\${\${ZERO:-\${0:#\$ZSH_ARGZERO}}:-\${(%):-%N}}"
-0="\${\${(M)0:#/*}:-\$PWD/\$0}"
+0=\${\${ZERO:-\${0:#\$ZSH_ARGZERO}}:-\${(%):-%N}}
+0=\${\${(M)0:#/*}:-\$PWD/\$0}
 
 # Then \${0:h} to get plugin's directory
 
