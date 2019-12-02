eb06d2d47bcc11224aa9ccb056bd3f897c18a79d
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-12-02 04:02:52 +0100

    *autoload: Advanced widget-unloading: recognize (match) more functions

--- zplugin-autoload.zsh
+++ zplugin-autoload.zsh
@@ -1021,11 +1021,11 @@ ZPLGM[EXTENDED_GLOB]=""
                     oth_orig_saved[5]="${(q)orig_saved3}" # chain up the widget
                     entry_splitted[found_idx]="${(q)${(j: :)oth_orig_saved}}"
                     ZPLGM[WIDGETS_SAVED__$oth_uspl2]="${(j: :)entry_splitted}"
-                    integer idx="${functions[$orig_saved2][(i)(#b)([^\{[:space:]]#$orig_saved1)]}"
+                    integer idx="${functions[$orig_saved2][(i)(#b)([^\{[:space:]]#$orig_saved1[^\{[:space:]])#]}"
                     if (( idx <= ${#functions[$orig_saved2]} ))
                     then
                         local prefix_X="${match[1]}"
-                        idx="${functions[$oth_fun][(i)(#b)([^\{[:space:]]#$orig_saved1)]}"
+                        idx="${functions[$oth_fun][(i)(#b)([^\{[:space:]]#$orig_saved1[^\}[:space:]]#)]}"
                         if (( idx <= ${#functions[$oth_fun]} )); then
                             local oth_prefix_uspl2_X="${match[1]}"
                             if [[ "${widgets[$prefix_X]}" = builtin ]]; then
