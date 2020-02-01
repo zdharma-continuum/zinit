8e7d5da6686bbeab7f4e52509c9b4a8f5a9f13cd
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-02-01 20:58:24 +0100

    sync.sh

--- sync.sh
+++ sync.sh
@@ -3,33 +3,33 @@
 autoload colors
 colors
 
-git -C ~/github/zplugin_readme checkout gh-pages || {
+git -C ~/github/readme_zplugin checkout gh-pages || {
     print $fg_bold[green]Couldn\'t checkout, trying stash first...$reset_color
     sleep 1
-    git -C ~/github/zplugin_readme stash save "$(LANG=C date)"
-    git -C ~/github/zplugin_readme checkout gh-pages || exit 1
+    git -C ~/github/readme_zplugin stash save "$(LANG=C date)"
+    git -C ~/github/readme_zplugin checkout gh-pages || exit 1
 }
-git -C ~/github/zplugin_readme llf
-git -C ~/github/zplugin_readme rebase || {
+git -C ~/github/readme_zplugin llf
+git -C ~/github/readme_zplugin rebase || {
     print $fg_bold[green]Couldn\'t rebase, trying stash first...$reset_color
-    git -C ~/github/zplugin_readme stash save "$(LANG=C date)" || exit 2
-    git -C ~/github/zplugin_readme rebase || exit 3
+    git -C ~/github/readme_zplugin stash save "$(LANG=C date)" || exit 2
+    git -C ~/github/readme_zplugin rebase || exit 3
 }
 
 print $fg_bold[green]Copying files...$reset_color
 sleep 1
-cp -f ~/github/zplugin_readme/docs/*.md docs
+cp -f ~/github/readme_zplugin/docs/*.md docs
 git add -A docs
-cp -fv ~/github/zplugin_readme/mkdocs.yml .
+cp -fv ~/github/readme_zplugin/mkdocs.yml .
 git add mkdocs.yml
-cp -fv ~/github/zplugin_readme/docs/css/* docs/css
+cp -fv ~/github/readme_zplugin/docs/css/* docs/css
 git add -A docs/css
 mkdir -p docs/js
-cp -fv ~/github/zplugin_readme/docs/js/* docs/js
+cp -fv ~/github/readme_zplugin/docs/js/* docs/js
 git add -A docs/js
-cp -fv ~/github/zplugin_readme/docs/img/* docs/img
+cp -fv ~/github/readme_zplugin/docs/img/* docs/img
 git add -A docs/img
-cp -vR ~/github/zplugin_readme/docs/theme/* docs/theme
+cp -vR ~/github/readme_zplugin/docs/theme/* docs/theme
 git add -A docs/theme
 print $fg_bold[green]Commiting:$reset_color
 sleep 1
