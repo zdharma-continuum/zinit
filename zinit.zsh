b3afd760615197b294ce727d1631924a79b2166e
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-02-26 09:04:49 +0100

    zinit: A lighter orange for col-msg2

--- zinit.zsh
+++ zinit.zsh
@@ -122,7 +122,7 @@ zmodload zsh/termcap 2>/dev/null
 
         col-pre    $'\e[38;5;141m'
         col-msg1   $'\e[0m'
-        col-msg2   $'\e[38;5;166m'
+        col-msg2   $'\e[38;5;172m'
         col-obj    $'\e[38;5;221m'
         col-obj2   $'\e[38;5;140m'
         col-file   $'\e[38;5;110m'
