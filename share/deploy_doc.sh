f7952e370b97952dfd24ac56458b4488babdea56
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-04-13 16:51:03 +0200

    deploy_doc.sh: Update

--- share/deploy_doc.sh
+++ share/deploy_doc.sh
@@ -73,7 +73,7 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY="${!ENCRYPTED_KEY_VAR}"
 ENCRYPTED_IV="${!ENCRYPTED_IV_VAR}"
 
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../share/deploy_key.enc -out ../share/deploy_key -d
+#openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../share/deploy_key.enc -out ../share/deploy_key -d
 chmod 600 ../share/deploy_key
 eval `ssh-agent -s`
 ssh-add ../share/deploy_key
