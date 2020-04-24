09b78e9646d4cbfe777e90648c3247947e1b7c9e
Mykola Rybak <mykola.rybak@gmail.com>
2020-04-24 13:41:45 +0300

    Fix completions installation condition
    Recently zinit started installing completions event if nocompletions
    ice is specified. This seems to be related to recent commit
    https://github.com/zdharma/zinit/commit/a46f97e8c37c51f63788599a2b2870f6f7c7a39e
    
    With that change, if either nocompletions or as"null" ices are not
    specified than completions are installed, so to avoid completions
    installation one has to specify both nocompletions _and_ as"null".
    
    This commit changes logic to be equivalent to what was before.

--- zinit-install.zsh
+++ zinit-install.zsh
@@ -539,7 +539,7 @@ builtin source "${ZINIT[BIN_DIR]}/zinit-side.zsh" || { print -P "${ZINIT[col-err
     # After additional executions like atclone'' - install completions (1 - plugins)
     local -A ICE_OPTS
     ICE_OPTS[opt_-q,--quiet]=1
-    [[ 0 = ${+ZINIT_ICE[nocompletions]} || ${ZINIT_ICE[as]} != null ]] && \
+    [[ 0 = ${+ZINIT_ICE[nocompletions]} && ${ZINIT_ICE[as]} != null ]] && \
         .zinit-install-completions "$id_as" "" "0"
 
     if [[ -e /tmp/zinit.skipped_comps.$$.lst || -e /tmp/zinit.installed_comps.$$.lst ]] {
@@ -1332,7 +1332,7 @@ builtin source "${ZINIT[BIN_DIR]}/zinit-side.zsh" || { print -P "${ZINIT[col-err
     # After additional executions like atclone'' - install completions (2 - snippets)
     local -A ICE_OPTS
     ICE_OPTS[opt_-q,--quiet]=1
-    [[ 0 = ${+ZINIT_ICE[nocompletions]} || ${ZINIT_ICE[as]} != null ]] && \
+    [[ 0 = ${+ZINIT_ICE[nocompletions]} && ${ZINIT_ICE[as]} != null ]] && \
         .zinit-install-completions "%" "$local_dir/$dirname" 0
 
     if [[ -e /tmp/zinit.skipped_comps.$$.lst || -e /tmp/zinit.installed_comps.$$.lst ]] {
