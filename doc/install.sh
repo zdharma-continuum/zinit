98c120db6412c19d9a24ed2c0a3515b22c7ac323
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-05-15 10:52:47 +0200

    install.sh: Reorder the annexes

--- doc/install.sh
+++ doc/install.sh
@@ -93,8 +93,8 @@ EOF
 # Load a few important annexes, without Turbo
 # (this is currently required for annexes)
 zinit light-mode for \\
-    zinit-zsh/z-a-patch-dl \\
     zinit-zsh/z-a-as-monitor \\
+    zinit-zsh/z-a-patch-dl \\
     zinit-zsh/z-a-bin-gem-node
 
 EOF
