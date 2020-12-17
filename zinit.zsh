c27bf53f060d4584333ebdff8cfcd6aed60ca430
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-12-16 19:06:38 -0600

    Updated the color theme.

--- zinit.zsh
+++ zinit.zsh
@@ -134,34 +134,37 @@ if [[ -z $SOURCED && ( ${+terminfo} -eq 1 && -n ${terminfo[colors]} ) || \
 ]] {
     ZINIT+=(
         # Old colors: 31m
-        col-pname   $'\e[1m\e[32m'       col-uname   $'\e[1m\e[35m'       col-keyword $'\e[32m'
-        col-note    $'\e[38;5;57m'       col-error   $'\e[1m\e[38;5;204m' col-p       $'\e[38;5;81m'
-        col-info    $'\e[38;5;82m'       col-info2   $'\e[38;5;227m' 
-        col-uninst  $'\e[38;5;118m'      col-info3   $'\e[1m\e[38;5;227m'
-        col-failure $'\e[38;5;204m'      col-happy   $'\e[1m\e[38;5;82m'
+        col-pname   $'\e[1;4m\e[32m'     col-uname   $'\e[1;4m\e[35m'     col-keyword $'\e[32m'
+        col-note    $'\e[38;5;148m'       col-error   $'\e[1m\e[38;5;204m' col-p       $'\e[38;5;81m'
+        col-info    $'\e[38;5;82m'       col-info2   $'\e[38;5;227m'      col-profile $'\e[38;5;148m'
+        col-uninst  $'\e[38;5;118m'      col-info3   $'\e[1m\e[38;5;227m' col-slight  $'\e[38;5;230m'
+        col-failure $'\e[38;5;204m'      col-happy   $'\e[1m\e[38;5;82m'  col-annex   $'\e[38;5;153m'
+        col-id-as   $'\e[4;38;5;220m'    col-version $'\e[3;38;5;87m'
         # The more recent, fresh ones:
         col-pre  $'\e[38;5;135m'  col-msg   $'\e[0m'        col-msg2  $'\e[38;5;172m'
         col-obj  $'\e[38;5;218m'  col-obj2  $'\e[38;5;118m' col-file  $'\e[38;5;117m'
+        col-dir  $'\e[3;38;5;153m'
         col-url  $'\e[38;5;75m'   col-meta  $'\e[38;5;57m'  col-meta2 $'\e[38;5;147m'
-        col-data $'\e[38;5;82m'   col-data2 $'\e[38;5;117m' col-hi    $'\e[1m\e[38;5;204m'
-        col-var  $'\e[38;5;81m'  col-glob  $'\e[38;5;227m' col-ehi   $'\e[1m\e[38;5;210m'
-        col-cmd  $'\e[38;5;82m'   col-ice   $'\e[38;5;27m'  col-nl    $'\n'
+        col-data $'\e[38;5;82m'   col-data2 $'\e[38;5;117m' col-hi    $'\e[1m\e[38;5;183m'
+        col-var  $'\e[38;5;81m'  col-glob   $'\e[38;5;227m' col-ehi   $'\e[1m\e[38;5;210m'
+        col-cmd  $'\e[38;5;82m'   col-ice   $'\e[38;5;39m'  col-nl    $'\n'
         col-txt  $'\e[38;5;254m'  col-num   $'\e[38;5;204m' col-term  $'\e[38;5;185m'
-        col-warn $'\e[38;5;214m'  col-apo   $'\e[38;5;220m' col-ok    $'\e[1m\e[38;5;220m'
+        col-warn $'\e[38;5;214m'  col-apo $'\e[1;38;5;220m' col-ok    $'\e[1m\e[38;5;220m'
         col-inv  $'\e[38;5;238m'  col-opt   $'\e[38;5;219m' col-lhi   $'\e[38;5;81m'
         col-tab  $'\t'            col-msg3  $'\e[38;5;238m' col-b-lhi $'\e[1m\e[38;5;75m'
         col-…    "${${${(M)LANG:#*UTF-8*}:+…}:-...}"  col-ndsh  "${${${(M)LANG:#*UTF-8*}:+–}:-}"
         col-mdsh "${${${(M)LANG:#*UTF-8*}:+–}:--}"    col-mmdsh "${${${(M)LANG:#*UTF-8*}:+――}:--}"
-        col--…   "${${${(M)LANG:#*UTF-8*}:+⋯⋯}:-···}" col-lr "${${${(M)LANG:#*UTF-8*}:+↔}:-"«-»"}"
-        col-rst  $'\e[0m'        col-b     $'\e[1m'        col-nb    $'\e[22m'
-        col-u    $'\e[4m'        col-it    $'\e[3m'        col-st    $'\e[9m'
-        col-nu   $'\e[24m'       col-nit   $'\e[23m'       col-nst   $'\e[29m'
-        col-bspc $'\b'
+        col--…   "${${${(M)LANG:#*UTF-8*}:+⋯⋯}:-···}" col-lr    "${${${(M)LANG:#*UTF-8*}:+↔}:-"«-»"}"
+        col-↔    ${${${(M)LANG:#*UTF-8*}:+$'\e[38;5;82m↔'}:-$'\e[38;5;82m<->'}
+        col-rst  $'\e[0m'        col-b     $'\e[1m'        col-nb     $'\e[22m'
+        col-u    $'\e[4m'        col-it    $'\e[3m'        col-st     $'\e[9m'
+        col-nu   $'\e[24m'       col-nit   $'\e[23m'       col-nst    $'\e[29m'
+        col-bspc $'\b'        col-b-warn $'\e[1;38;5;214m' col-u-warn $'\e[4;38;5;214m'
     )
     if [[ ( ${+terminfo} -eq 1 && ${terminfo[colors]} -ge 256 ) || \
           ( ${+termcap} -eq 1 && ${termcap[Co]} -ge 256 )
     ]] {
-        ZINIT+=( col-pname $'\e[1m\e[38;5;39m' col-uname  $'\e[1m\e[38;5;207m' )
+        ZINIT+=( col-pname $'\e[1;4m\e[38;5;39m' col-uname  $'\e[1;4m\e[38;5;207m' )
     }
 }
 
