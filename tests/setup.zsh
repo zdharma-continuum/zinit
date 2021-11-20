3ac21a9ba16fd626e7a5cce27f3d7565126762f0
Philipp Schmitt <philipp@schmitt.co>
2021-11-20 21:54:49 +0100

    Test the sbin ice

--- tests/setup.zsh
+++ tests/setup.zsh
@@ -4,12 +4,14 @@ setup() {
   export DATA_DIR="${TMPDIR:-/tmp}/zunit-zinit"
   export PLUGINS_DIR="${DATA_DIR}/plugins"
   export SNIPPETS_DIR="${DATA_DIR}/snippets"
+  export ZPFX="${DATA_DIR}/polaris"
 
   {
     color magenta @setup started
     color magenta "DATA_DIR=${DATA_DIR}"
     color magenta "PLUGINS_DIR=${PLUGINS_DIR}"
     color magenta "SNIPPETS_DIR=${SNIPPETS_DIR}"
+    color magenta "ZPFX=${ZPFX}"
   } >&2
 
   # Recreate DATA_DIR
