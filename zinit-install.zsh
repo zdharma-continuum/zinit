6f039dfaee1915f0d0d00b9c26636ee7011959f1
Dusty Phillips <86920+dusty-phillips@users.noreply.github.com>
2022-03-22 10:01:58 -0300

    Fix zinit-jq-check command in zinit-get-package
    This is failing for lack of a `.`.

--- zinit-install.zsh
+++ zinit-install.zsh
@@ -64,7 +64,7 @@ builtin source "${ZINIT[BIN_DIR]}/zinit-side.zsh" || {
 
 # FUNCTION: .zinit-get-package [[[
 .zinit-get-package() {
-    zinit-jq-check || return 1
+    .zinit-jq-check || return 1
 
     emulate -LR zsh
     setopt extendedglob warncreateglobal typesetsilent noshortloops rcquotes
