d9117cf5423c5aab0b92da0209912bc6ff9629c9
Vladislav Doster <mvdoster@gmail.com>
2022-07-01 04:08:09 -0500

    style: re-format rpm2cpio.zsh to zinit convention
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- share/rpm2cpio.zsh
+++ share/rpm2cpio.zsh
@@ -1,12 +1,17 @@
 #!/usr/bin/env zsh
+#
+# -*- mode: sh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+#
+# Copyright (c) 2016-2020 Sebastian Gniazdowski and contributors
+# Copyright (c) 2021-2022 zdharma-continuum and contributors
 
 emulate -R zsh -o extendedglob
 
 local pkg=$1
-if [[ -z $pkg || ! -e $pkg ]] {
-    print -u2 -Pr "%F{160}rpm2cpio.sh%f: no package supplied"
-    exit 1
-}
+if [[ -z $pkg || ! -e $pkg ]]; then
+  print -u2 -Pr "%F{160}rpm2cpio.sh%f: no package supplied"
+  exit 1
+fi
 
 local leadsize=96
 local o=$(( $leadsize + 8 ))
@@ -28,24 +33,24 @@ UNPACKCMD=( dd if=$pkg ibs=$o skip=1 )
 local COMPRESSION="$($=UNPACKCMD | file -)"
 local -a DECOMPRESSCMD
 
-if [[ $COMPRESSION == (#i)*gzip* ]] {
-    DECOMPRESSCMD=( gunzip )
-} elif [[ $COMPRESSION == (#i)*bzip2* ]] {
-    DECOMPRESSCMD=( bunzip2 )
-} elif [[ $COMPRESSION == (#i)*xz* ]] {
-    DECOMPRESSCMD=( unxz )
-} elif [[ $COMPRESSION == (#i)*cpio* ]] {
-    DECOMPRESSCMD=( cat )
-} else {
-    DECOMPRESSCMD=( $(which unlzma 2>/dev/null) )
-    if [[ $DECOMPRESSCMD != /* ]] {
-        DECOMPRESSCMD=( $(which lzmash 2>/dev/null) )
-        if [[ $DECOMPRESSCMD == /* ]] {
-            DECOMPRESSCMD=( lzmash -d -c )
-        } else {
-            DECOMPRESSCMD=( cat )
-        }
-    }
-}
+if [[ $COMPRESSION == (#i)*gzip* ]]; then
+  DECOMPRESSCMD=( gunzip )
+elif [[ $COMPRESSION == (#i)*bzip2* ]]; then
+  DECOMPRESSCMD=( bunzip2 )
+elif [[ $COMPRESSION == (#i)*xz* ]]; then
+  DECOMPRESSCMD=( unxz )
+elif [[ $COMPRESSION == (#i)*cpio* ]]; then
+  DECOMPRESSCMD=( cat )
+else
+  DECOMPRESSCMD=( $(which unlzma 2>/dev/null) )
+  if [[ $DECOMPRESSCMD != /* ]]; then
+    DECOMPRESSCMD=( $(which lzmash 2>/dev/null) )
+    if [[ $DECOMPRESSCMD == /* ]]; then
+      DECOMPRESSCMD=( lzmash -d -c )
+    else
+      DECOMPRESSCMD=( cat )
+    fi
+  fi
+fi
 
 command "$UNPACKCMD[@]" 2>/dev/null | command "$DECOMPRESSCMD[@]"
