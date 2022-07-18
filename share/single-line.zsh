6598216d3a90b84740bdb7cb70636fdffe9c9f40
Vladislav Doster <mvdoster@gmail.com>
2022-07-18 11:10:07 -0500

    style: formatting
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- share/single-line.zsh
+++ share/single-line.zsh
@@ -5,23 +5,17 @@
 
 emulate -R zsh
 setopt extendedglob noshortloops rcquotes typesetsilent warncreateglobal
-
 local IFS= n=$'\n' r=$'\r' zero=$'\0'
-
 {
-  command perl -pe 'BEGIN { $|++; $/ = \1 }; tr/\r\n/\n\0/' \
-    || gstdbuf -o0 gtr '\r\n' '\n\0' \
-    || stdbuf -o0 tr '\r\n' '\n\0';
+  command perl -pe 'BEGIN { $|++; $/ = \1 }; tr/\r\n/\n\0/' || gstdbuf -o0 gtr '\r\n' '\n\0' || stdbuf -o0 tr '\r\n' '\n\0'
   print
-} 2>/dev/null | while read -r line;
+} 2> /dev/null | while read -r line
 do
-  if [[ $line == *$zero* ]]; then
-    # cURL doesn't add a newline to progress bars
-    # print -nr -- "${r}${(l:COLUMNS:: :):-}${r}${line##*${zero}}"
+  if [[ $line == *$zero* ]]
+  then
     print -nr -- "${r}${(l:COLUMNS:: :):-}${r}${line%${zero}}"
   else
     print -nr -- "${r}${(l:COLUMNS:: :):-}${r}${${line//[${r}${n}]/}%\%*}${${(M)line%\%}:+%}"
   fi
 done
-
 print
