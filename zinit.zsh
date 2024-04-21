fffd1a53f451b0a5d550e5405acbf9bdca0726b3
Vladislav Doster <mvdoster@gmail.com>
2024-04-21 18:49:52 -0500

    wip: playing around with printing
    ```zsh
    time ( 'print' '-P' '${(@kv)"${(@kP)ZINIT[colors]}"[glob]}' 'asdsas' '${(@kv)"${(@kP)ZINIT[colors]}"[rst]}' )
    ```
    
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- zinit.zsh
+++ zinit.zsh
@@ -203,25 +203,25 @@ zmodload zsh/term{cap,info} 2>/dev/null
 autoload -Uz colors && colors
 
 if [[ -z $SOURCED && ( ${+terminfo} -eq 1 && -n ${terminfo[colors]} ) || ( ${+termcap} -eq 1 && -n ${termcap[Co]} ) ]]; then
-  ZINIT+=(
-    col-annex   $'\e[38;5;153m'         col-faint   $'\e[38;5;238m'         col-msg2    $'\e[38;5;172m'      col-quo     $'\e[1;38;5;33m'
-    col-apo     $'\e[1;38;5;45m'        col-file    $'\e[3;38;5;117m'       col-msg3    $'\e[38;5;238m'      col-quos    $'\e[1;38;5;160m'
-    col-aps     $'\e[38;5;117m'         col-flag    $'\e[1;3;38;5;79m'      col-nb      $'\e[22m'            col-rst     $'\e[0m'
-    col-b       $'\e[1m'                col-func    $'\e[38;5;219m'         col-nit     $'\e[23m'            col-slight  $'\e[38;5;230m'
-    col-b-lhi   $'\e[1m\e[38;5;75m'     col-glob    $'\e[38;5;227m'         col-nl      $'\n'                col-st      $'\e[9m'
-    col-b-warn  $'\e[1;38;5;214m'       col-happy   $'\e[1m\e[38;5;82m'     col-note    $'\e[38;5;148m'      col-tab     $' \t '
-    col-bapo    $'\e[1;38;5;220m'       col-hi      $'\e[1m\e[38;5;183m'    col-nst     $'\e[29m'            col-term    $'\e[38;5;185m'
-    col-baps    $'\e[1;38;5;82m'        col-ice     $'\e[38;5;39m'          col-nu      $'\e[24m'            col-th-bar  $'\e[38;5;82m'
-    col-bar     $'\e[38;5;82m'          col-id-as   $'\e[4;38;5;220m'       col-num     $'\e[3;38;5;155m'    col-time    $'\e[38;5;220m'
-    col-bcmd    $'\e[38;5;220m'         col-info    $'\e[38;5;82m'          col-obj     $'\e[38;5;218m'      col-txt     $'\e[38;5;254m'
-    col-bspc    $'\b'                   col-info2   $'\e[38;5;227m'         col-obj2    $'\e[38;5;118m'      col-u       $'\e[4m'
-    col-cmd     $'\e[38;5;82m'          col-info3   $'\e[1m\e[38;5;227m'    col-ok      $'\e[38;5;220m'      col-u-warn  $'\e[4;38;5;214m'
-    col-data    $'\e[38;5;82m'          col-it      $'\e[3m'                col-opt     $'\e[38;5;219m'      col-uname   $'\e[1;4m\e[35m'
-    col-data2   $'\e[38;5;117m'         col-keyword $'\e[32m'               col-p       $'\e[38;5;81m'       col-uninst  $'\e[38;5;118m'
-    col-dir     $'\e[3;38;5;153m'       col-lhi     $'\e[38;5;81m'          col-pkg     $'\e[1;3;38;5;27m'   col-url     $'\e[38;5;75m'
-    col-ehi     $'\e[1m\e[38;5;210m'    col-meta    $'\e[38;5;57m'          col-pname   $'\e[1;4m\e[32m'     col-var     $'\e[38;5;81m'
-    col-error   $'\e[1m\e[38;5;204m'    col-meta2   $'\e[38;5;147m'         col-pre     $'\e[38;5;135m'      col-version $'\e[3;38;5;87m'
-    col-failure $'\e[38;5;204m'         col-msg     $'\e[0m'                col-profile $'\e[38;5;148m'      col-warn    $'\e[38;5;214m'
+  typeset -HA __zinit_color=(
+    annex   $'\e[38;5;153m'         faint   $'\e[38;5;238m'         msg2    $'\e[38;5;172m'      quo     $'\e[1;38;5;33m'
+    apo     $'\e[1;38;5;45m'        file    $'\e[3;38;5;117m'       msg3    $'\e[38;5;238m'      quos    $'\e[1;38;5;160m'
+    aps     $'\e[38;5;117m'         flag    $'\e[1;3;38;5;79m'      nb      $'\e[22m'            rst     $'\e[0m'
+    b       $'\e[1m'                func    $'\e[38;5;219m'         nit     $'\e[23m'            slight  $'\e[38;5;230m'
+    b-lhi   $'\e[1m\e[38;5;75m'     glob    $'\e[38;5;227m'         nl      $'\n'                st      $'\e[9m'
+    b-warn  $'\e[1;38;5;214m'       happy   $'\e[1m\e[38;5;82m'     note    $'\e[38;5;148m'      tab     $' \t '
+    bapo    $'\e[1;38;5;220m'       hi      $'\e[1m\e[38;5;183m'    nst     $'\e[29m'            term    $'\e[38;5;185m'
+    baps    $'\e[1;38;5;82m'        ice     $'\e[38;5;39m'          nu      $'\e[24m'            th-bar  $'\e[38;5;82m'
+    bar     $'\e[38;5;82m'          id-as   $'\e[4;38;5;220m'       num     $'\e[3;38;5;155m'    time    $'\e[38;5;220m'
+    bcmd    $'\e[38;5;220m'         info    $'\e[38;5;82m'          obj     $'\e[38;5;218m'      txt     $'\e[38;5;254m'
+    bspc    $'\b'                   info2   $'\e[38;5;227m'         obj2    $'\e[38;5;118m'      u       $'\e[4m'
+    cmd     $'\e[38;5;82m'          info3   $'\e[1m\e[38;5;227m'    ok      $'\e[38;5;220m'      u-warn  $'\e[4;38;5;214m'
+    data    $'\e[38;5;82m'          it      $'\e[3m'                opt     $'\e[38;5;219m'      uname   $'\e[1;4m\e[35m'
+    data2   $'\e[38;5;117m'         keyword $'\e[32m'               p       $'\e[38;5;81m'       uninst  $'\e[38;5;118m'
+    dir     $'\e[3;38;5;153m'       lhi     $'\e[38;5;81m'          pkg     $'\e[1;3;38;5;27m'   url     $'\e[38;5;75m'
+    ehi     $'\e[1m\e[38;5;210m'    meta    $'\e[38;5;57m'          pname   $'\e[1;4m\e[32m'     var     $'\e[38;5;81m'
+    error   $'\e[1m\e[38;5;204m'    meta2   $'\e[38;5;147m'         pre     $'\e[38;5;135m'      version $'\e[3;38;5;87m'
+    failure $'\e[38;5;204m'         msg     $'\e[0m'                profile $'\e[38;5;148m'      warn    $'\e[38;5;214m'
 
     col-dbg $'\e[2m\e[38;47;107m'"[debug]"$'\e[0m'
     col-e $'\e[1m\e[38;5;204m'"Error"$'\e[0m'":"
@@ -238,8 +238,9 @@ if [[ -z $SOURCED && ( ${+terminfo} -eq 1 && -n ${terminfo[colors]} ) || ( ${+te
     col-↔     ${${${(M)LANG:#*UTF-8*}:+$'\e[38;5;82m↔\e[0m'}:-$'\e[38;5;82m«-»\e[0m'}
   )
   if [[ ( ${+terminfo} -eq 1 && ${terminfo[colors]} -ge 256 ) || ( ${+termcap} -eq 1 && ${termcap[Co]} -ge 256 ) ]]; then
-    ZINIT+=( col-pname $'\e[1;4m\e[38;5;39m' col-uname  $'\e[1;4m\e[38;5;207m' )
+    __zinit_color+=( pname $'\e[1;4m\e[38;5;39m' uname  $'\e[1;4m\e[38;5;207m' )
   fi
+  ZINIT+=(colors '__zinit_color')
 fi
 
 # Hooks
